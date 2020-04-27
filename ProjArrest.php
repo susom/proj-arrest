05feadbaf8a9387a7fa2bf0da4b999ba71f26255
Andrew Martin <andy123@stanford.edu>
2020-04-27 11:02:04 -0700

    cleanup

--- ProjArrest.php
+++ ProjArrest.php
@@ -22,6 +22,162 @@ class ProjArrest extends \ExternalModules\AbstractExternalModule {
 
 
     /**
+     * SAVE RECORD HOOK
+     * @param $project_id
+     * @param $record
+     * @param $instrument
+     * @param $event_id
+     * @param $group_id
+     * @param $survey_hash
+     * @param $response_id
+     * @param $repeat_instance
+     */
+	public function redcap_save_record($project_id, $record, $instrument, $event_id, $group_id,
+                                       $survey_hash, $response_id, $repeat_instance) {
+        $this->emDebug("Save on $instrument in $event_id");
+
+        if ($this->inRandomEvent($record,$event_id)) {
+
+            // See if we need to update the study id
+            $result = $this->checkStudyId($record, $group_id);
+            $this->emDebug($result);
+
+            // See if we need to update the pharma alias
+            $result = $this->checkPharmaAlias($record, $group_id);
+            $this->emDebug($result);
+
+        }
+
+    }
+
+
+    /**
+     * Assigns a study id if record is randomized and missing a study id
+     * Should only be called if the current event_id is the randomiation event
+     * @param $record
+     * @param $event_id
+     * @param $group_id
+     * @return bool|string
+     */
+    public function checkStudyId($record, $group_id) {
+
+        if ($this->random_result !== "") {
+            // The record has been randomized
+            // Check if it has a study_id - we do this as a second query as it doesn't necessarily
+            // have to be in the same event as the first query
+            $study_id_field = $this->getProjectSetting('study-name-field');
+            $study_id_event = $this->getProjectSetting('study-name-event');
+            if ($this->getValue($record, $study_id_field, $study_id_event) === "") {
+                // Let's create a study_id
+
+                // First, let's get the dagNum
+                $dagNum = $this->getDagGroupIdToDagNumPrefix($group_id);
+                if ($dagNum == false) {
+                    $this->emLog("Unable to get a valid dagNum for $record / $group_id");
+                    return false;
+                }
+
+                // Next, let's get the ID:
+                $prefix        = "P" . $dagNum . "-";
+                $padding       = 3;
+                $next_study_id = $this->getNextStudyId($prefix, $padding, $study_id_field, $study_id_event);
+
+                if ($next_study_id == false) {
+                    $this->emLog("There was an error getting the nextStudyId for $record with $prefix");
+                    return false;
+                }
+
+                // Let's write the ID to the record
+                $data   = array(
+                    $record => array(
+                        $study_id_event => array(
+                            $study_id_field => $next_study_id
+                        )
+                    )
+                );
+                $result = REDCap::saveData('array', $data);
+                if(!empty($result['errors'])) {
+                    $this->emError("Unable to save new study id", $result);
+                    return false;
+                }
+                return $next_study_id;
+            }
+        }
+    }
+
+
+    /**
+     * This pulls an unused alias from a separate project and assigns it to a record in this project
+     * @param $record
+     * @param $group_id
+     * @return bool
+     */
+    private function checkPharmaAlias($record, $group_id) {
+        // See if we need to get a pharma alias
+
+        // If not randomized, we do not need to do anything.
+        if (empty($this->random_result)) return false;
+
+        $pharma_alias_field = $this->getProjectSetting('pharma-alias-field');
+        $pharma_alias_event = $this->getProjectSetting('pharma-alias-event');
+        $pharma_alias_pid   = $this->getProjectSetting('pharma-alias-pid');
+
+        // Get the current alias
+        $current_alias = $this->getValue($record, $pharma_alias_field, $pharma_alias_event);
+
+        // If already has an alias, we don't need to do anything.
+        if (!empty($current_alias)) return false;
+
+        // Lookup the next alias
+        $site = $this->getDagGroupIdToDagNumPrefix($group_id);
+
+        $params = array(
+            "project_id"    => $pharma_alias_pid,
+            "return_format" => 'json',
+            "filterLogic"   => "[used_by] = '' AND [group] = '$this->random_result' AND [site] = '$site'"
+        );
+        $q = REDCap::getData($params);
+        $results = json_decode($q,true);
+
+        $this->emDebug("Found " . count($results) . " results");
+        if (count($results) == 0) {
+            // No more available
+            $this->emLog("Unable to find pharma alias with query", $params);
+            return false;
+        } else {
+            $result = $results[0];
+
+            // Reserve this alias
+            $result['used_by'] = $record;
+            $q = REDCap::saveData($pharma_alias_pid, 'json', json_encode(array($result)));
+            $this->emDebug("save result", $q);
+            if (!empty($q['errors'])) {
+                REDCap::logEvent("Unable to set pharma codebook alias!  Check server logs for details.","","",$record);
+                $this->emError("Unable to save pharma alias", $result, $q);
+                return false;
+            }
+
+            // Save the alias to this record
+            $code = $result['code'];
+            $data   = array(
+                $record => array(
+                    $pharma_alias_event => array(
+                        $pharma_alias_field => $code
+                    )
+                )
+            );
+            $result = REDCap::saveData('array', $data);
+            if(!empty($result['errors'])) {
+                $this->emError("Unable to save new study id", $result);
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+
+    /**
      * Convert the dag name to a numerical id from 1 to 10 for this study
      * e.g. 05_duke => 5
      * @return array
@@ -92,152 +248,6 @@ class ProjArrest extends \ExternalModules\AbstractExternalModule {
     }
 
 
-
-    /**
-     * Assigns a study id if record is randomized and missing a study id
-     * Should only be called if the current event_id is the randomiation event
-     * @param $record
-     * @param $event_id
-     * @param $group_id
-     * @return bool|string
-     */
-    public function checkStudyId($record, $group_id) {
-
-        if ($this->random_result !== "") {
-            // The record has been randomized
-            // Check if it has a study_id - we do this as a second query as it doesn't necessarily
-            // have to be in the same event as the first query
-            $study_id_field = $this->getProjectSetting('study-name-field');
-            $study_id_event = $this->getProjectSetting('study-name-event');
-            if ($this->getValue($record, $study_id_field, $study_id_event) === "") {
-                // Let's create a study_id
-
-                // First, let's get the dagNum
-                $dagNum = $this->getDagGroupIdToDagNumPrefix($group_id);
-                if ($dagNum == false) {
-                    $this->emLog("Unable to get a valid dagNum for $record / $group_id");
-                    return false;
-                }
-
-                // Next, let's get the ID:
-                $prefix        = "P" . $dagNum . "-";
-                $padding       = 3;
-                $next_study_id = $this->getNextStudyId($prefix, $padding, $study_id_field, $study_id_event);
-
-                if ($next_study_id == false) {
-                    $this->emLog("There was an error getting the nextStudyId for $record with $prefix");
-                    return false;
-                }
-
-                // Let's write the ID to the record
-                $data   = array(
-                    $record => array(
-                        $study_id_event => array(
-                            $study_id_field => $next_study_id
-                        )
-                    )
-                );
-                $result = REDCap::saveData('array', $data);
-                if(!empty($result['errors'])) {
-                    $this->emError("Unable to save new study id", $result);
-                    return false;
-                }
-                return $next_study_id;
-            }
-        }
-    }
-
-
-
-	public function redcap_save_record($project_id, $record, $instrument, $event_id, $group_id,
-                                       $survey_hash, $response_id, $repeat_instance) {
-        $this->emDebug("Save on $instrument in $event_id");
-
-        if ($this->inRandomEvent($record,$event_id)) {
-
-            // See if we need to update the study id
-            $result = $this->checkStudyId($record, $group_id);
-            $this->emDebug($result);
-
-            // See if we need to update the pharma alias
-            $result = $this->checkPharmaAlias($record, $group_id);
-            $this->emDebug($result);
-
-        }
-
-    }
-
-
-    private function checkPharmaAlias($record, $group_id) {
-        // See if we need to get a pharma alias
-
-        // If not randomized, we do not need to do anything.
-        if (empty($this->random_result)) return false;
-
-        $pharma_alias_field = $this->getProjectSetting('pharma-alias-field');
-        $pharma_alias_event = $this->getProjectSetting('pharma-alias-event');
-        $pharma_alias_pid   = $this->getProjectSetting('pharma-alias-pid');
-
-        // Get the current alias
-        $current_alias = $this->getValue($record, $pharma_alias_field, $pharma_alias_event);
-
-        // If already has an alias, we don't need to do anything.
-        if (!empty($current_alias)) return false;
-
-        // Lookup the next alias
-        $site = $this->getDagGroupIdToDagNumPrefix($group_id);
-
-        $params = array(
-            "project_id"    => $pharma_alias_pid,
-            "return_format" => 'json',
-            "filterLogic"   => "[used_by] = '' AND [group] = '$this->random_result' AND [site] = '$site'"
-        );
-        $q = REDCap::getData($params);
-        $results = json_decode($q,true);
-
-        $this->emDebug("Found " . count($results) . " results");
-        if (count($results) == 0) {
-            // No more available
-            $this->emLog("Unable to find pharma alias with query", $params);
-            return false;
-        } else {
-            $result = $results[0];
-
-            // Reserve this alias
-            $result['used_by'] = $record;
-            $q = REDCap::saveData($pharma_alias_pid, 'json', json_encode(array($result)));
-            $this->emDebug("save result", $q);
-            if (!empty($q['errors'])) {
-                REDCap::logEvent("Unable to set pharma codebook alias!  Check server logs for details.","","",$record);
-                $this->emError("Unable to save pharma alias", $result, $q);
-                return false;
-            }
-
-            // Save the alias to this record
-            $code = $result['code'];
-            $data   = array(
-                $record => array(
-                    $pharma_alias_event => array(
-                        $pharma_alias_field => $code
-                    )
-                )
-            );
-            $result = REDCap::saveData('array', $data);
-            if(!empty($result['errors'])) {
-                $this->emError("Unable to save new study id", $result);
-                return false;
-            }
-
-            return true;
-        }
-    }
-
-
-
-
-
-
-
     /**
      * Look up a value from the database
      * @param $record
@@ -256,8 +266,6 @@ class ProjArrest extends \ExternalModules\AbstractExternalModule {
         $result = isset($q[$record][$event_id][$field_name]) ? $q[$record][$event_id][$field_name] : false;
         return $result;
     }
-
-
 
 
     /**
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ProjArrest
-A template for a REDCap External Module
+# Proj Arrest
+This is a custom em for the Arrest project with Haley Hedlin.
+
+1. When the randomization is complete, it assigns a secondary id field based on the dag name.
+
+1. When the randomization is complete, it assignes a pharma alias from a separate pharma prohect and marks that alias
+ as used.
+
